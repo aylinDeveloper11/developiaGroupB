b56b7b01260e73b7e7c305b0f39a1c69560f329f
Aylin <mahisuleyman@gmail.com>
2025-02-14 21:15:10 +0400

    fifth commit

--- homeWork/src/Encapsulation2/Main.java
+++ homeWork/src/Encapsulation2/Main.java
@@ -1,5 +1,14 @@
 package Encapsulation2;
 
 public class Main {
-
+	public static void main(String[] args) {
+		Person person = new Person();
+		person.setName("Aylin");
+		person.setAge(12);
+		
+		
+		System.out.println(person.getName());
+		System.out.println(person.getAge());
+		
+	}
 }
