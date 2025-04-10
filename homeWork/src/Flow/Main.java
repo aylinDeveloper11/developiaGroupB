c0efd3c1118d8cba4638ef831498bc5b603cd849
Aylin <mahisuleyman@gmail.com>
2025-04-10 11:00:49 +0400

    twenty-two commit

---
+++ homeWork/src/Flow/Main.java
@@ -0,0 +1,17 @@
+package Flow;
+
+
+public class Main {
+public static void main(String[] args) {
+	Cleaning cleaning = new Cleaning();
+	cleaning.start();
+	
+	Podcast podcast = new Podcast();
+	podcast.start();
+	
+	Thread eating = new Thread(new Eating());
+	eating.start();
+	
+	
+	}
+}
